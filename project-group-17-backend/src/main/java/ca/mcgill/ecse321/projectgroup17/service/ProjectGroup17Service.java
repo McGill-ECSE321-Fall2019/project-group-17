9fbeb38b772a7d788cfcdb990549238d2887a398
Felix Simard <felixsimard@gmail.com>
2019-10-09 13:44:05 -0400

    Updated imports in Service Class

--- project-group-17-backend/src/main/java/ca/mcgill/ecse321/projectgroup17/service/ProjectGroup17Service.java
+++ project-group-17-backend/src/main/java/ca/mcgill/ecse321/projectgroup17/service/ProjectGroup17Service.java
@@ -13,11 +13,42 @@ import ca.mcgill.ecse321.projectgroup17.dao.AvailabilityRepository;
 import ca.mcgill.ecse321.projectgroup17.model.Course;
 import ca.mcgill.ecse321.projectgroup17.model.Person;
 import ca.mcgill.ecse321.projectgroup17.model.Availability;
+import ca.mcgill.ecse321.projectgroup17.model.Appointment;
+import ca.mcgill.ecse321.projectgroup17.model.Course;
+import ca.mcgill.ecse321.projectgroup17.model.Level;
+import ca.mcgill.ecse321.projectgroup17.model.Review;
+import ca.mcgill.ecse321.projectgroup17.model.Room;
+import ca.mcgill.ecse321.projectgroup17.model.SpecificCourse;
+import ca.mcgill.ecse321.projectgroup17.model.Student;
+import ca.mcgill.ecse321.projectgroup17.model.Tutor;
 
+
+@Service
 public class ProjectGroup17Service {
 
-	
-	
-	
-	
+	@Autowired
+	EventRepository eventRepository;
+	@Autowired
+	PersonRepository personRepository;
+	@Autowired
+	RegistrationRepository registrationRepository;
+
+	@Transactional
+	public Person createPerson(String name) {
+		Person person = new Person();
+		person.setName(name);
+		personRepository.save(person);
+		return person;
+	}
+
+	@Transactional
+	public Person getPerson(String name) {
+		Person person = personRepository.findPersonByName(name);
+		return person;
+	}
+
+
+
+
+
 }
