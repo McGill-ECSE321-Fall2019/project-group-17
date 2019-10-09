7e94a0cd7285937f90819dd6493b612f4a98f2d6
Felix Simard <felixsimard@gmail.com>
2019-10-09 13:31:26 -0400

    Main Service Class Added

---
+++ project-group-17-backend/src/main/java/ca/mcgill/ecse321/projectgroup17/service/ProjectGroup17Service.java
@@ -0,0 +1,23 @@
+package ca.mcgill.ecse321.projectgroup17.service;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.projectgroup17.dao.AvailabilityRepository;
+import ca.mcgill.ecse321.projectgroup17.model.Course;
+import ca.mcgill.ecse321.projectgroup17.model.Person;
+import ca.mcgill.ecse321.projectgroup17.model.Availability;
+
+public class ProjectGroup17Service {
+
+	
+	
+	
+	
+}
