9c32e6c7078ea88c9f3f6211ce12c2e83b8a880b
CharlesBourbeau <bourbeau.charles99@gmail.com>
2019-11-11 19:02:42 -0500

    Starting the review page

---
+++ project-group-17-frontend/src/components/Review.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="review">
+    <div class="inner_container shadow">
+      
+      <div class="large_text left_text">Create Review</div>
+
+</table>
+
+    </div>
+  
+
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+
+</style>
