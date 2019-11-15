021d02c261b6d41d3c3dd1e420f06a370fc79b93
KyleDouglasMyers <kyle.myers@mail.mcgill.ca>
2019-11-14 20:05:51 -0500

    ViewStudentReviews (20%)

---
+++ project-group-17-frontend/src/components/viewStudentReviews.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import Vue from 'vue'
+import StarRating from 'vue-star-rating' 
+
+//constructors
+
+
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+    baseURL: backendUrl,
+    headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+export default {
+	name: 'viewStudentReviews',
+	data() {
+		return{
+			reviews: [],
+			students: []
+
+		}
+	},
+	created: function(){
+
+		const s1 = new StudentDto("Jim", "Molson")
+		const s2 = new StudentDto("Joe", "Morrison")
+		this.testStudents = [s1, s2]
+		var appt_id = this.$parent.appt_id_review
+		//we have the appointemnt id in app, so we can get the students from that
+		AXIOS.get(backendUrl+'/persons/getStudentsByAppointmentID?appointmentID='+appt_id)
+      	.then(response => {
+        // JSON responses are automatically parsed.
+        	this.students = response.data
+        	this.errorReview = ''
+      	})
+      	.catch(e => {
+        	var errorMsg = e.message
+			console.log(errorMsg)
+			this.errorReview = errorMsg	
+      	});
+	},
+	methods: {
+
+	}
+}
