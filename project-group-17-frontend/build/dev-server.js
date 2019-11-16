0dec67db5e2f9010824a6c4c15ac09a1432b83ee
Felix Simard <felixsimard@gmail.com>
2019-11-16 17:40:21 -0500

    Updated dev-server.js

--- project-group-17-frontend/build/dev-server.js
+++ project-group-17-frontend/build/dev-server.js
@@ -65,7 +65,9 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var host = config.dev.host
+//var host = config.dev.host
+var host = config.build.host
+
 var uri = 'http://' + host + ':' + port
 
 var _resolve
