0decffd775f66c409fc7df4042389d00820d9a06
Felix Simard <felixsimard@gmail.com>
2019-11-21 18:16:11 -0500

    Created HttpUtils.java

--- project-group-17-android/app/src/main/java/ca/mcgill/ecse321/project_group_17_android/MainActivity.java
+++ project-group-17-android/app/src/main/java/ca/mcgill/ecse321/project_group_17_android/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,8 +15,52 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    //
+    private String error = null;
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void login(View v) {
+        error = "";
+        final TextView username = (TextView) findViewById(R.id.login_username);
+        final TextView password = (TextView) findViewById(R.id.login_password);
+        HttpUtils.post("/persons/getByUsername/?username=" + username.getText().toString() + "&password=" + password.getText().toString(), new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                System.out.println(response);
+                username.setText("");
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +77,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // initialize error message text view
+        refreshErrorMessage();
     }
 
     @Override
